5fec869fe643d40f38d53e4db752995f3b06795b
ahmedAnany <ahmedenany9812@gmail.com>
2026-03-14 13:20:06 +0200

    feat: Initialize Next.js project with base layout, global styles, favicon, and tsconfig path alias.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <button className="bg-white text-black px-4 py-2 rounded-md cursor-pointer">Click me</button>
+    </div>
+  );
+}
