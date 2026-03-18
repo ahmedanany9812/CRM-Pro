2ace226b595407c66a0ae019ef86ad9550f4b8a2
ahmedAnany <ahmedenany9812@gmail.com>
2026-03-18 12:08:21 +0200

    feat: add AppSidebar for application navigation and introduce a reusable Avatar component.

--- src/components/sidebar/AppSidebar.tsx
+++ src/components/sidebar/AppSidebar.tsx
@@ -16,6 +16,7 @@ import {
 } from "../ui/sidebar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export default function AppSidebar() {
   const pathname = usePathname();
@@ -34,15 +35,18 @@ export default function AppSidebar() {
     <Sidebar>
       <SidebarHeader>
         <div className="p-4">
-          <div className="flex items-center gap-2">
+          <div className="flex items-start flex-col gap-1">
             <span className="text-lg font-semibold">Whispyr</span>
+            <span className="text-muted-foreground text-sm">Admin</span>
           </div>
         </div>
       </SidebarHeader>
       <Separator />
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Main</SidebarGroupLabel>
+          <SidebarGroupLabel className="text-muted-foreground">
+            MAIN
+          </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu className="space-y-3">
               {mainSidebarItems.map((item) => (
@@ -58,8 +62,43 @@ export default function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-muted-foreground">
+            ADMINISTRATION
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="space-y-3">
+              {adminSidebarItems.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton asChild isActive={isActive(item.href)}>
+                    <Link href={item.href}>
+                      <item.icon className="size-4" />
+                      <span>{item.label}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <Separator />
+        <div className="p-4">
+          <div className="flex items-center gap-2">
+            <Avatar size="lg">
+              {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+              <AvatarFallback>AA</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col items-start gap-1">
+              <span className="text-md font-medium">Ahmed Anany</span>
+              <span className="text-sm text-muted-foreground">
+                ahmed98@gmail.com
+              </span>
+            </div>
+          </div>
+        </div>
+      </SidebarFooter>
     </Sidebar>
   );
 }
